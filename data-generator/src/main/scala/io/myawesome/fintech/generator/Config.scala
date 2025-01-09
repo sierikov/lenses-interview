87a63e3a88c3482e25ee79af55719fe4983e8c55
sierikov <sierikov@pm.me>
2025-01-09 14:40:52 +0100

    Generator sends random clicks data to Kafka

---
+++ data-generator/src/main/scala/io/myawesome/fintech/generator/Config.scala
@@ -0,0 +1,7 @@
+package io.myawesome.fintech.generator
+
+// TODO: make with pureconfig
+object Config {
+  val bootstrapServers = "localhost:9092"
+  val schemaRegistryUrl = "http://localhost:8081"
+}
--- data-generator/src/main/scala/io/myawesome/fintech/generator/Main.scala
+++ data-generator/src/main/scala/io/myawesome/fintech/generator/Main.scala
@@ -1,16 +1,40 @@
 package io.myawesome.fintech.generator
 
-import cats.effect.{IO, IOApp}
+
+import cats.effect.*
 import cats.effect.std.Random
+import cats.syntax.all.*
+import vulcan.Codec
+import vulcan.generic.*
 import fs2.Stream
+import fs2.kafka.*
+import fs2.kafka.vulcan.*
+import io.myawesome.fintech.common.ClickRecord
 import org.typelevel.log4cats.Logger
 import org.typelevel.log4cats.slf4j.Slf4jLogger
 
-import scala.concurrent.duration.DurationInt
+import scala.concurrent.duration.*
 
 object Main extends IOApp.Simple {
 
   given logger: Logger[IO] = Slf4jLogger.getLogger[IO]
+
+
+  private val avroSettings = AvroSettings {
+    SchemaRegistryClientSettings[IO](Config.schemaRegistryUrl)
+  }
+
+  import io.myawesome.fintech.common.VulcanUnionTypeOrderEnforcer.*
+
+  given Codec[ClickRecord] = Codec.derived[ClickRecord]
+
+  given Resource[IO, ValueSerializer[IO, ClickRecord]] = avroSerializer[ClickRecord].forValue(avroSettings)
+
+  given Resource[IO, KeySerializer[IO, String]] = avroSerializer[String].forKey(avroSettings)
+
+  private val producerSettings = ProducerSettings[IO, String, ClickRecord]
+    .withBootstrapServers(Config.bootstrapServers)
+    .withProperty("client.id", "fintech.generator")
 
   override def run: IO[Unit] = program
 
@@ -25,11 +49,17 @@ object Main extends IOApp.Simple {
   }
 
   private def stream(generator: RandomClickRecordGenerator[IO]): Stream[IO, Unit] = {
-    Stream.awakeEvery[IO](5.seconds)
-      .evalMap { _ =>
-        generator.generate.flatMap { record =>
-          logger.info(s"Generated ClickRecord: $record")
-        }
+    KafkaProducer.stream(producerSettings).flatMap { producer =>
+        Stream.awakeEvery[IO](5.seconds).evalMap { _ =>
+          generator.generate.flatMap { record =>
+            logger.info(s"Produced $record") *>
+              producer.produce(
+                ProducerRecords.one(ProducerRecord("test-click-topic4", record.session_id, record))
+              )
+          }
+        }.drain
       }
+      .parJoinUnbounded
+      .handleErrorWith { case ex: Exception => Stream.emit(logger.error(s"Failed to send record! Error: ${ex.getMessage}")) }
   }
 }
